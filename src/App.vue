3a5d0cc4ecd1b10b10718f15ae8a83c258fbc009
yoptohlejepeta <peta.kotlan@seznam.cz>
2025-01-28 21:54:43 +0100

    project init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Button from "primevue/button";
+</script>
+
+<template>
+  <h1>
+    VUE test
+  </h1>
+  <Button label="Test button" raised/>
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'light',
+        }
+    }
+});
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
